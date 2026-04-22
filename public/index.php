3b7fb3c53ecfbbd3eccc8551cc7a33786475927c
Drxpz08 <rodrigomiguelbgomes@gmail.com>
2026-04-22 14:43:52 +0100

    Criação da pasta para a API e para o projeto.

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$uri = str_replace("mydevpiratas.com/public", "", $uri);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($uri === '/' || $uri === '/index' || $uri === '/home') {
+  var_dump("Entrar no index raiz home");
+} elseif ($uri === '/login' && $method === "GET") {
+  var_dump("Entrar na página login");
+} elseif ($uri === '/login' && $method === "POST") {
+  var_dump("Entrar na página login post para submeter");
+}  elseif ($uri === '/sigup' && $method === "GET") {
+  var_dump("Entrar na página sigup");
+} elseif ($uri === '/signup' && $method === "POST") {
+  var_dump("Entrar na página signup post para submeter");
+}
+elseif($uri === '/users' && $method === "GET") {
+  var_dump("Entrar na página users");
+} else {
+  echo "404";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
